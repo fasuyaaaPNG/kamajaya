79a7cd9cbc9a0c128fe53bf50211aa44c9f03c5a
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-01-02 14:23:39 +0700

    upload

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,34 @@
+import "./style.css"
+
+export default function Login() {
+    return (
+        <>
+           <div className="container">
+                <div className="content">
+                    <div className="text">
+                        <p>
+                            Login to your account
+                        </p>
+                    </div>
+                    <form action="">
+                        <div className="input">
+                            <input type="email" className="email" placeholder="Email" />
+                            <input type="password" className="password" placeholder="Password" />
+                        </div>
+                        <div className="submit">
+                            <button className="btn login">
+                                Login
+                            </button>
+                        </div>
+                        <div className="continue">
+                            <img src="/login/google.png" alt="" className="google" />
+                            <button className="btn cgoogle">
+                                Continue with Google
+                            </button>
+                        </div>
+                    </form>
+                </div>
+           </div>
+        </>
+    )
+}
