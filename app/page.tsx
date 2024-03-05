40613e744a9b9608599f828577442e91e7b3b44a
YoruEXE <rayaandresty@gmail.com>
2024-03-05 19:02:17 +0700

    change navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,19 @@ import "./page.module.css"
 export default function Home() {
   return (
       
-    <header className="header">
-      <nav className="navbar">
-        <div className="navbar-container">
-          <ul className="nav-item">
-            <li>
-              <a> Beranda </a>
-            </li>
-            <li>
-              <a> Tentang </a>
-            </li>
-            <li>
-              <a> Daftar </a>
-            </li>
-            <li>
-              <a> HALOooooo </a>
-            </li>
-          </ul>
+<header className="header">
+    <nav className="navbar">
+      <div className="navbar-container container">
+        <div>
+          <h1 className="navbar-brand">Roronoa Zoro</h1>
         </div>
-      </nav>
-    </header>
-    
+        <ul className="menu-items">
+          <li><a href="#about">About</a></li>
+          <li><a href="#my-works">Profile</a></li>
+          <li><a href="#contact-me">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+</header>  
   )
 }
