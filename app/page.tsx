bd71d5c84253b3dea5bafe8f457c5a101836521c
YoruEXE <rayaandresty@gmail.com>
2024-03-06 11:28:04 +0700

    home

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,17 @@ export default function Home() {
         </ul>
       </div>
     </nav>
+
+    <div className="home-content" id="home-page">
+      <div className="name">
+        <h1>Hi, I'm ロロノア・ ゾロ</h1>
+        <p>The Strongest Swordsman</p>
+      </div>
+      <div className="angle-down-icon">
+        <a href="#about"><i className="fas fa-angle-down"></i></a>
+      </div>
+    </div>
+
 </header>
 </body> 
   )
