634c304cfd40a4b92681102f24d250b9487807fb
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-03-07 07:36:08 +0700

    nav

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <ul className="menu-items">
           <li><a href="#beranda">Beranda</a></li>
           <li><a href="#tentang">Tentang</a></li>
-          <li><a href="#daftar">Daftar</a></li>
+          <li><a className="daftar" href="#daftar">Daftar</a></li>
         </ul>
       </div>
     </nav>
